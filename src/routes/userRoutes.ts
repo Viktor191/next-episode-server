44cc103dac139582b7433722b15f6b298c3db00b
Viktor191 <109589393+Viktor191@users.noreply.github.com>
2025-01-10 21:04:42 +0100

    fix rout in userRoutes.ts

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,11 +2,11 @@ import {Router} from "express";
 
 const router= Router();
 
-router.get("/user/me", (req,res    ) => {
+router.get("/me", (req,res    ) => {
     res.status(200).send('response /user/me');
 })
 
-router.get("/user/me/favorites", (req,res    ) => {
+router.get("/me/favorites", (req,res    ) => {
     console.log('response /user/me/favorites');
     res.status(200).send('response /user/me/favorites');
 })
