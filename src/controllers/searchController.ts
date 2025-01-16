58fc79b5956505241b48ad08c285dd0183fa39a7
Viktor191 <109589393+Viktor191@users.noreply.github.com>
2025-01-16 17:11:45 +0100

    added search filtering by title to make the results shorter

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -3,6 +3,14 @@ import axios from "axios";
 
 const TMDB_API_URL = process.env.TMDB_API_URL;
 
+type SelectedMovieResult = {
+    id: number;
+    name: string;
+    overview: string;
+    first_air_date: string | null;
+    vote_average: number;
+};
+
 export const searchMovie = async (req: Request, res: Response): Promise<void> => {
     try {
         const {query} = req.query;
@@ -20,9 +28,22 @@ export const searchMovie = async (req: Request, res: Response): Promise<void> =>
                 Authorization: `Bearer ${process.env.TMDB_BEARER_TOKEN}`,
             },
         });
-        console.log(response.data);
+        const results = response.data.results;
 
-        res.status(200).json(response.data)
+        if (!results || results.length === 0) {
+            res.status(404).json({error: 'Ничего не найдено'});
+            return;
+        }
+
+        const simplifiedResults: SelectedMovieResult[] = results.map((result: any) => ({
+            id: result.id,
+            name: result.title || result.original_title,
+            overview: result.overview,
+            first_air_date: result.release_date || null,
+            vote_average: result.vote_average,
+        }));
+
+        res.status(200).json(simplifiedResults);
     } catch (error: any) {
         console.error('Ошибка при поиске фильма:', error.message);
 
